90070e346ca4d9ad032881a480c51fe6f60be73b
Prakhar Verma <prakharverma2019@gmail.com>
2023-10-20 13:29:18 +0530

    modified:   calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Calculator{
+    private:
+    long a;
+    long b;
+    public:
+    Calculator(long a, char op, long b){}
+    long add(long )
+};
+int main(){
+
+    return 0;
+}
