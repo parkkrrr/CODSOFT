efca67745c7e99a88a4772c4b6965e80da2a1af6
Prakhar Verma <prakharverma2019@gmail.com>
2023-10-20 15:42:37 +0530

    modified:   .vscode/settings.json 	modified:   calculator.cpp 	new file:   ticketBookingSystem.cpp 	new file:   tictactoe.cpp 	new file:   tictactoe.exe 	new file:   todoList.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -9,47 +9,59 @@ private:
 
 public:
     Calculator(double a, double b) {}
-    Calculator(){}
+    Calculator() {}
     double add(double a, double b) { return a + b; }
     double div(double a, double b) { return (b != 0) ? a / b : 0; }
     double mul(double a, double b) { return a * b; }
     double sub(double a, double b) { return a - b; }
 
-    void exp(string result){
-        char arr[]={'+','*','/','-'};
-        for(int i:arr){
-            if(result.find(i)){
-                switch(i){
-                case '+': add(a,b);
-                            break;
-                case '-': sub(a,b);
-                            break;
-                case '*': mul(a,b);
-                            break;
-                case '/': div(a,b);
-                default: cout<<"Invalid Input";
+    void exp(string result)
+    {
+        char arr[] = {'+', '*', '/', '-'};
+        for (int i : arr)
+        {
+            if (result.find(i))
+            {
+                switch (i)
+                {
+                case '+':
+                    add(a, b);
+                    break;
+                case '-':
+                    sub(a, b);
+                    break;
+                case '*':
+                    mul(a, b);
+                    break;
+                case '/':
+                    div(a, b);
+                default:
+                    cout << "Invalid Input";
                 };
             }
+        }
     }
 };
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     Calculator calc;
     string result;
-    
-    if(argc>1){
-        result=argv[1];
-        cout<<calc.exp(result)<<endl;
+
+    if (argc > 1)
+    {
+        result = argv[1];
+        cout << calc.exp(result) << endl;
     }
-    else{
-        while(true){
-            cout<<"Enter expression or 'q' to quit -> ";
-            getline(cin,result);
-            if(result=='q'||result=='Q')
+    else
+    {
+        while (true)
+        {
+            cout << "Enter expression or 'q' to quit -> ";
+            getline(cin, result);
+            if (result == 'q' || result == 'Q')
                 break;
-            
+            cout << calc.exp(result) << endl;
         }
     }
     return 0;
---
+++ todoList.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+enum class Priority
+{
+    low,
+    high,
+    medium
+};
+
+class Task
+{
+    public:
+    int id;     //randomly generated;
+    string title;
+    bool completed;
+    Priority priority;
+    Task(string title, Priority priority, bool completed = false)
+    {
+        this->title = title;
+        this->priority = priority;
+    }
+    Task(){}
+};
+
+class ToDoList:private Task
+{
+public:
+    void add();
+    void remove();
+    void update();
+    void displayMenu();
+    void markComplete(Task task);
+    void completedTask();
+    void list();
+    
+    void add(){
+        cin>>title;
+        cin>>priority;
+        Task *temp=new Task(id,title,priority);
+        task=temp;
+    }
+
+    void displayMenu(){
+        cout<<"-------TO-DO-LIST-------\n";
+        cout<<"1.Add\n2.Remove\n3.Update\n4.Mark Complete\n5.Completed Task\n6. Display all Task\n7.Exit\n";
+
+        int op;
+        cin>>op;
+        switch(op){
+            case 1: add();
+                    break;
+            case 2: remove();
+                    break;
+            case 3: update();
+                    break;
+            case 4: markComplete();
+                    break;
+            case 5: completedTask();
+                    break;
+            case 6: list();
+                    break;
+            case 7: break;
+
+            default:cout<<"You have entered Wrong option.\n";
+        }
+    }
+
+    void update(){
+        list();
+        cout<<"Select ID"
+    }
+};
+int main(int argc, char *argv[])
+{
+    vector<Task> task;
+    ToDoList todo;
+    if(argc<2){
+        todo.displayMenu();
+        
+    }
+
+    return 0;
+}
