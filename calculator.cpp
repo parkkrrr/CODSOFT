6e9f86b20e498643ab94eb03938d808eaece4c2f
Prakhar Verma <prakharverma2019@gmail.com>
2023-10-20 15:42:20 +0530

    modified:   calculator.cpp 	modified:   .vscode/settings.json 	modified:   calculator.cpp 	ticketBookingSystem.cpp 	tictactoe.cpp 	tictactoe.exe 	todoList.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Calculator{
-    private:
-    long a;
-    long b;
-    public:
-    Calculator(long a, char op, long b){}
-    long add(long )
-};
-int main(){
+class Calculator
+{
+private:
+    double a;
+    double b;
 
+public:
+    Calculator(double a, double b) {}
+    Calculator(){}
+    double add(double a, double b) { return a + b; }
+    double div(double a, double b) { return (b != 0) ? a / b : 0; }
+    double mul(double a, double b) { return a * b; }
+    double sub(double a, double b) { return a - b; }
+
+    void exp(string result){
+        char arr[]={'+','*','/','-'};
+        for(int i:arr){
+            if(result.find(i)){
+                switch(i){
+                case '+': add(a,b);
+                            break;
+                case '-': sub(a,b);
+                            break;
+                case '*': mul(a,b);
+                            break;
+                case '/': div(a,b);
+                default: cout<<"Invalid Input";
+                };
+            }
+    }
+};
+
+
+int main(int argc, char* argv[])
+{
+    Calculator calc;
+    string result;
+    
+    if(argc>1){
+        result=argv[1];
+        cout<<calc.exp(result)<<endl;
+    }
+    else{
+        while(true){
+            cout<<"Enter expression or 'q' to quit -> ";
+            getline(cin,result);
+            if(result=='q'||result=='Q')
+                break;
+            
+        }
+    }
     return 0;
 }
