eb6a55747e3360f732cfb85879dc97b1cc96a46c
Prakhar Verma <prakharverma2019@gmail.com>
2023-10-28 21:56:40 +0530

    modified:   .vscode/settings.json 	modified:   calculator.cpp 	new file:   calculator.exe

--- calculator.cpp
+++ calculator.cpp
@@ -3,66 +3,71 @@ using namespace std;
 
 class Calculator
 {
-private:
-    double a;
-    double b;
-
 public:
-    Calculator(double a, double b) {}
-    Calculator() {}
-    double add(double a, double b) { return a + b; }
-    double div(double a, double b) { return (b != 0) ? a / b : 0; }
-    double mul(double a, double b) { return a * b; }
-    double sub(double a, double b) { return a - b; }
+    double op1;
+    double op2;
 
-    void exp(string result)
-    {
-        char arr[] = {'+', '*', '/', '-'};
-        for (int i : arr)
-        {
-            if (result.find(i))
-            {
-                switch (i)
-                {
-                case '+':
-                    add(a, b);
-                    break;
-                case '-':
-                    sub(a, b);
-                    break;
-                case '*':
-                    mul(a, b);
-                    break;
-                case '/':
-                    div(a, b);
-                default:
-                    cout << "Invalid Input";
-                };
-            }
-        }
-    }
+    Calculator() : op1(0), op2(0) {}
+
+    double add() { return op1 + op2; }
+
+    double sub() { return op1 - op2; }
+
+    double mul() { return op1 * op2; }
+
+    double division() { return (op2 != 0) ? op1 / op2 : (exit(1), 0.0); }
 };
 
 int main(int argc, char *argv[])
 {
-    Calculator calc;
-    string result;
+    while (1)
+    {
+        Calculator calc;
+        char op;
 
-    if (argc > 1)
-    {
-        result = argv[1];
-        cout << calc.exp(result) << endl;
-    }
-    else
-    {
-        while (true)
+        if (argc == 4)
         {
-            cout << "Enter expression or 'q' to quit -> ";
-            getline(cin, result);
-            if (result == 'q' || result == 'Q')
-                break;
-            cout << calc.exp(result) << endl;
+            calc.op1 = atof(argv[1]);
+            calc.op2 = atof(argv[3]);
+            op = *argv[2];
         }
+        else
+        {
+            system("cls");
+            cout << "-----------Calculator-----------\n";
+            cout << "(+) Enter an expression -> ";
+
+            if (!(std::cin >> calc.op1 >> op >> calc.op2))
+            {
+                cout << "\n(-) Error\n";
+                return 1;
+            }
+        }
+
+        double result;
+
+        switch (op)
+        {
+        case '+':
+            result = calc.add();
+            break;
+        case '-':
+            result = calc.sub();
+            break;
+        case '*':
+            result = calc.mul();
+            break;
+        case '/':
+            result = calc.division();
+            break;
+        default:
+            cout << "\n(-) Invalid operator-> " << op << "\n";
+            return 1;
+        }
+
+        cout << "\n(+) Result -> " << result << "\n";
     }
+
+    system("pause");
     return 0;
 }
